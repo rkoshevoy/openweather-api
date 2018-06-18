68e5a8b1a589ed6793b4ceaed15a8ca331c4bc2f
Ruslan Koshevoy <ruslan.koshevoj@gmail.com>
2018-06-18 17:24:42 +0300

    temporary view of weekly weather

--- js/owapi.js
+++ js/owapi.js
@@ -15,12 +15,24 @@ $(document).ready(function() {
         type: 'GET',
         dataType: 'jsonp',
         success: function(data) {
-          var widget = showCurrentWeather(data);
-          $('#showWeather').html(widget);
+          var currentWeatherWidget = showCurrentWeather(data);
+          $('#showCurrentWeather').html(currentWeatherWidget);
           $('#city').val('');
         },
         error: function() {
           alert('Wrong city name. You must enter name in english')
+        }
+      });
+
+      $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + city +
+          '&units=metric' +
+          '&APPID=8eb3e1f5948dfaa9255d77de0dc82b42',
+        type: 'GET',
+        dataType: 'jsonp',
+        success: function(data) {
+          var weeklyWeatherWidget = showWeeklyWeather(data);
+          $('#showWeeklyWeather').html(weeklyWeatherWidget);
         }
       });
 
@@ -40,8 +52,20 @@ $(document).ready(function() {
       type: 'GET',
       dataType: 'jsonp',
       success: function(data) {
-        var widget = showCurrentWeather(data);
-        $('#showWeather').html(widget);
+        var currentWeatherWidget = showCurrentWeather(data);
+        $('#showCurrentWeather').html(currentWeatherWidget);
+      }
+    });
+
+    $.ajax({
+      url: 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude +
+        '&units=metric' +
+        '&APPID=8eb3e1f5948dfaa9255d77de0dc82b42',
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function(data) {
+        var weeklyWeatherWidget = showWeeklyWeather(data);
+        $('#showWeeklyWeather').html(weeklyWeatherWidget);
       }
     });
   }
@@ -49,19 +73,19 @@ $(document).ready(function() {
   navigator.geolocation.getCurrentPosition(getCoordinates);
 });
 
+function timeFormatting(time) {
+  var date = new Date(time * 1000);
+  var hours = date.getHours();
+  var minutes = "0" + date.getMinutes();
+  var seconds = "0" + date.getSeconds();
+  var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+  return formattedTime;
+};
+
 function showCurrentWeather(data) {
 
-  function timeFormatting(time) {
-    var date = new Date(time * 1000);
-    var hours = date.getHours();
-    var minutes = "0" + date.getMinutes();
-    var seconds = "0" + date.getSeconds();
-    var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
-    return formattedTime;
-  };
-
-  return '<p><strong>Weather in ' + data.name + ', ' + data.sys.country + '</strong></p>' +
+  var currentWeatherData = '<p><strong>Current weather in ' + data.name + ', ' + data.sys.country + '</strong></p>' +
     '<p><img src="//openweathermap.org/img/w/' + data.weather[0].icon + '.png">' + parseInt(data.main.temp) + '°' +
     '</p>' +
     '<tr>' +
@@ -90,5 +114,49 @@ function showCurrentWeather(data) {
     '<td>Sunset</td>' +
     '<td>' + timeFormatting(data.sys.sunset) + '</td>' +
     '</tr>' +
-    '</table>'
+    '</table>';
+
+  return currentWeatherData;
+};
+
+function showWeeklyWeather(data) {
+
+  var weeklyWeatherCityInfo = '<p><strong>Weekly weather in ' + data.city.name + ', ' + data.city.country + '</strong></p>';
+  var weeklyWeatherData = [];
+
+  for (var i = 0; i < data.list.length; i++) {
+    var weeklyWeatherDataItem = '<p><img src="//openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png">' + parseInt(data.list[i].main.temp) + '°' +
+      '</p>' +
+      '<tr>' +
+      '<table>' +
+      '<tr>' +
+      '<td>Cloudiness</td>' +
+      '<td>' + data.list[i].weather[0].main + '</td>' +
+      '</tr>' +
+      '<tr>' +
+      '<td>Wind speed</td>' +
+      '<td>' + data.list[i].wind.speed + ' m/s</td>' +
+      '</tr>' +
+      '<tr>' +
+      '<td>Pressure</td>' +
+      '<td>' + data.list[i].main.pressure + ' hpa</td>' +
+      '</tr>' +
+      '<tr>' +
+      '<td>Humidity</td>' +
+      '<td>' + data.list[i].main.humidity + '%</td>' +
+      '</tr>' +
+      '<tr>' +
+      '<td>Humidity</td>' +
+      '<td>' + data.list[i].main.humidity + '%</td>' +
+      '</tr>' +
+      '<tr>' +
+      '<td>Time</td>' +
+      '<td>' + data.list[i].dt_txt + '</td>' +
+      '</tr>' +
+      '</table>';
+
+    weeklyWeatherData.push(weeklyWeatherDataItem);
+  }
+
+  return weeklyWeatherCityInfo + weeklyWeatherData;
 };

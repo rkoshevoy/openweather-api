4f38d8c93fbadd08bd1b7261be605d58c6576601
Ruslan Koshevoy <ruslan.koshevoj@gmail.com>
2018-06-18 13:45:53 +0300

    some updates & added current position weather

--- js/owapi.js
+++ js/owapi.js
@@ -29,13 +29,39 @@ $(document).ready(function(){
       $('#error').html('<div class="alert alert-warning alert-dismissible fade show" role="alert">Field can not be empty<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
     }
   })
+
+  function getCoordinates(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    $.ajax({
+      url: 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude +
+      '&units=metric' +
+      '&APPID=8eb3e1f5948dfaa9255d77de0dc82b42',
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function(data){
+        var widget = show(data);
+
+        $('#showWeather').html(widget);
+        $('#city').val('');
+      }
+    });
+  }
+
+  function errorGetCoordinates() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+
+  navigator.geolocation.getCurrentPosition(getCoordinates, errorGetCoordinates);
 });
 
 function show(data) {
   return '<p><strong>Weather in ' + data.name + '</strong></p>' +
-        '<p>Main weather: ' + data.weather[0].main + '</p>' +
-        '<p>Main weather description: ' + data.weather[0].description + '</p>' +
-        '<p>Temperature: ' + data.main.temp + '</p>' +
-        '<p>Pressure: ' + data.main.pressure + '</p>'+
-        '<p>Humidity: ' + data.main.humidity + '</p>'
-}
+
+        '<p><img src="//openweathermap.org/img/w/' + data.weather[0].icon +'.png">' + '</p>' +
+        '<p>Temperature: ' + parseInt(data.main.temp) + '°</p>' +
+        '<p>Pressure: ' + data.main.pressure + ' Pa</p>'+
+        '<p>Humidity: ' + data.main.humidity + '%</p>'+
+        '<p>Wind speed: ' + data.wind.speed + ' kph</p>'
+};

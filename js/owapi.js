42f111a1f155617e22bf373a2d50ac01ab765f9f
Ruslan Koshevoy <ruslan.koshevoj@gmail.com>
2018-06-18 14:39:00 +0300

    soryan, last update was excess

--- js/owapi.js
+++ js/owapi.js
@@ -42,15 +42,13 @@ $(document).ready(function(){
       dataType: 'jsonp',
       success: function(data){
         var widget = show(data);
+
+        $('#showWeather').html(widget);
       }
     });
   }
 
-  function errorGetCoordinates() {
-    output.innerHTML = "Unable to retrieve your location";
-  }
-
-  navigator.geolocation.getCurrentPosition(getCoordinates, errorGetCoordinates);
+  navigator.geolocation.getCurrentPosition(getCoordinates);
 });
 
 function show(data) {
